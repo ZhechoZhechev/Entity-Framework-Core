658d5325889f76f21458604f5590df5ec1acd0e0
ZhechoZhechev <mishomishev024@gmail.com>
2023-02-16 14:31:26 +0200

    ADO.NET Lab

---
+++ ADONET_Lab/ADONET/Program.cs
@@ -0,0 +1,27 @@
+﻿
+
+using Microsoft.Data.SqlClient;
+
+SqlConnection connection = new SqlConnection("Server=.;Database=SoftUni;User Id=sa;Password=8201291108;Trusted_Connection=False;MultipleActiveResultSets=true; TrustServerCertificate=true");
+
+connection.Open();
+
+using (connection) 
+{
+    SqlCommand cmd = new SqlCommand("SELECT * FROM Employees WHERE DepartmentId = 5", connection);
+
+    SqlDataReader reader = cmd.ExecuteReader();
+
+    using (reader)
+    {
+        while (reader.Read())
+        {
+            string? firstName = reader["FirstName"].ToString();
+            string? lastName = reader["LastName"].ToString();
+            decimal salary = (decimal)reader["Salary"];
+
+            Console.WriteLine($"{firstName} {lastName} has ${salary} salary");
+        }
+    }
+}
+
