e3dc2d19a0d539fbfe4debbf9764e5060fd11cb6
ZhechoZhechev <mishomishev024@gmail.com>
2023-03-02 17:43:25 +0200

    LINQ Exercise

---
+++ LINQ_Exersice/01. MusicHub Database/MusicHub/Data/Models/Writer.cs
@@ -0,0 +1,20 @@
+﻿namespace MusicHub.Data.Models;
+
+using System.Collections;
+using System.ComponentModel.DataAnnotations;
+
+
+public class Writer
+{
+    [Key]
+    public int Id { get; set; }
+
+    [Required]
+    [StringLength(20)]
+    public string Name { get; set; } = null!;
+
+    public string? Pseudonym { get; set; }
+
+    public virtual ICollection<Song> Songs { get; set; } = null!;
+
+}
