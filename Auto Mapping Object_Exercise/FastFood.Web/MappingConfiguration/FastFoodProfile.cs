d06705f5ec8017fce0822c10effe6eaf13fc81e6
ZhechoZhechev <mishomishev024@gmail.com>
2023-03-09 17:23:15 +0200

    Auto Mapping Workshop

---
+++ Auto Mapping Object_Exercise/FastFood.Web/MappingConfiguration/FastFoodProfile.cs
@@ -0,0 +1,49 @@
+﻿using FastFood.Web.ViewModels.Categories;
+
+namespace FastFood.Web.MappingConfiguration
+{
+    using AutoMapper;
+    using FastFood.Web.ViewModels.Employees;
+    using FastFood.Web.ViewModels.Items;
+    using FastFood.Web.ViewModels.Orders;
+    using Models;
+
+    using ViewModels.Positions;
+
+    public class FastFoodProfile : Profile
+    {
+        public FastFoodProfile()
+        {
+            //Positions
+            this.CreateMap<CreatePositionInputModel, Position>()
+                .ForMember(x => x.Name, y => y.MapFrom(s => s.PositionName));
+
+            this.CreateMap<Position, PositionsAllViewModel>()
+                .ForMember(x => x.Name, y => y.MapFrom(s => s.Name));
+
+            this.CreateMap<CreateCategoryInputModel, Category>()
+                .ForMember(x => x.Name, y => y.MapFrom(s => s.CategoryName));
+            this.CreateMap<Category, CategoryAllViewModel>()
+                .ForMember(x => x.Name, y => y.MapFrom(s => s.Name));
+
+            this.CreateMap<RegisterEmployeeInputModel, Employee>();      
+            this.CreateMap<Employee, EmployeesAllViewModel>()
+                .ForMember(x => x.Position, y => y.MapFrom(s => s.Position.Name));
+            this.CreateMap<Position, RegisterEmployeeViewModel>()
+                .ForMember(x => x.PositionId, y => y.MapFrom(s => s.Id));
+
+            this.CreateMap<CreateItemInputModel, Item>();
+            this.CreateMap<Item, ItemsAllViewModels>()
+                .ForMember(x => x.Category, y=> y.MapFrom(s => s.Id));
+          
+            this.CreateMap<Category, CreateItemViewModel>()
+                .ForMember(x => x.CategoryId, y => y.MapFrom(s => s.Id));
+
+               this.CreateMap<CreateOrderInputModel, Order>();
+            this.CreateMap<Order, OrderAllViewModel>()
+                .ForMember(x => x.Employee, y => y.MapFrom(s => s.Id));
+     
+
+        }
+    }
+}
