a985bc548f3960644d74183cfa9ddd4ecaa60ecb
ZhechoZhechev <mishomishev024@gmail.com>
2023-03-13 11:35:57 +0200

    JSON Exercise

---
+++ JSON_Exercise/09. Import Suppliers/CarDealer/StartUp.cs
@@ -0,0 +1,34 @@
+﻿using CarDealer.Data;
+using CarDealer.Models;
+using Newtonsoft.Json;
+
+namespace CarDealer;
+
+public class StartUp
+{
+    public static void Main()
+    {
+        CarDealerContext context = new CarDealerContext();
+
+        //context.Database.EnsureDeleted();
+        //context.Database.EnsureCreated();
+
+        //Console.WriteLine("Successfully created!");
+
+        string inpitJson = File.ReadAllText("../../../Datasets/suppliers.json");
+        string output = ImportSuppliers(context, inpitJson);
+        Console.WriteLine(output);
+    }
+
+    public static string ImportSuppliers(CarDealerContext context, string inputJson) 
+    {
+        ICollection<Supplier> supliersInfo;
+
+        supliersInfo = JsonConvert.DeserializeObject<List<Supplier>>(inputJson)!;
+
+        context.Suppliers.AddRange(supliersInfo);
+        context.SaveChanges();
+
+        return $"Successfully imported {supliersInfo.Count}.";
+    }
+}
---
+++ JSON_Exercise/10. Import Parts/CarDealer/StartUp.cs
@@ -0,0 +1,51 @@
+﻿using CarDealer.Data;
+using CarDealer.Models;
+using Newtonsoft.Json;
+using System.IO;
+
+namespace CarDealer;
+
+public class StartUp
+{
+    public static void Main()
+    {
+        CarDealerContext context = new CarDealerContext();
+
+        //context.Database.EnsureDeleted();
+        //context.Database.EnsureCreated();
+
+        //Console.WriteLine("Successfully created!");
+
+        string inpitJson = File.ReadAllText("../../../Datasets/parts.json");
+        string output = ImportParts(context, inpitJson);
+        Console.WriteLine(output);
+    }
+
+    public static string ImportSuppliers(CarDealerContext context, string inputJson)
+    {
+        ICollection<Supplier> supliersInfo;
+
+        supliersInfo = JsonConvert.DeserializeObject<List<Supplier>>(inputJson)!;
+
+        context.Suppliers.AddRange(supliersInfo);
+        context.SaveChanges();
+
+        return $"Successfully imported {supliersInfo.Count}.";
+    }
+    public static string ImportParts(CarDealerContext context, string inputJson)
+    {
+        var supliersId = context.Suppliers
+            .Select(s => s.Id)
+            .ToArray();
+
+        ICollection<Part> partsInfo;
+        partsInfo = JsonConvert.DeserializeObject<List<Part>>(inputJson)!
+            .Where(x => supliersId.Contains(x.SupplierId))
+            .ToList();
+
+        context.Parts.AddRange(partsInfo);
+        context.SaveChanges ();
+
+        return $"Successfully imported {partsInfo.Count}.";
+    }
+}
