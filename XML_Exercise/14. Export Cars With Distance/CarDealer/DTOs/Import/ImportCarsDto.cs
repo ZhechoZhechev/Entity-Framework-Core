da25939ad9658a17a36d900bdbf192cef21e2cd2
ZhechoZhechev <mishomishev024@gmail.com>
2023-03-17 11:33:02 +0200

    XML Exercise

---
+++ XML_Exercise/14. Export Cars With Distance/CarDealer/DTOs/Import/ImportCarsDto.cs
@@ -0,0 +1,26 @@
+﻿namespace CarDealer.DTOs.Import;
+
+using System.Xml.Serialization;
+
+[XmlType("Car")]
+public class ImportCarsDto
+{
+    [XmlElement("make")]
+    public string Make { get; set; }
+
+    [XmlElement("model")]
+    public string Model { get; set; }
+
+    [XmlElement("traveledDistance")]
+    public long TravellDistance { get; set; }
+
+    [XmlArray("parts")]
+    public PartDto[] PartIds { get; set; }
+}
+
+[XmlType("partId")]
+public class PartDto 
+{
+    [XmlAttribute("id")]
+    public int PartId { get; set; }
+}
---
+++ XML_Exercise/15. Export Cars From Make BMW/CarDealer/DTOs/Import/ImportCustomersDto.cs
@@ -0,0 +1,16 @@
+﻿using System.Xml.Serialization;
+
+namespace CarDealer.DTOs.Import;
+
+[XmlType("Customer")]
+public class ImportCustomersDto
+{
+    [XmlElement("name")]
+    public string Name { get; set; }
+
+    [XmlElement("birthDate")]
+    public string BirthDate { get; set; }
+
+    [XmlElement("isYoungDriver")]
+    public bool isYoungDriver { get; set; }
+}
