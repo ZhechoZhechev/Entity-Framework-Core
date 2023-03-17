dffa1c3a99b56a53c19659336ce6073f3eb36117
ZhechoZhechev <mishomishev024@gmail.com>
2023-03-17 15:49:45 +0200

    XML Exercise

---
+++ XML_Exercise/19. Export Sales With Applied Discount/CarDealer/DTOs/Export/ExportTotalSalesByCustomer.cs
@@ -0,0 +1,21 @@
+﻿namespace CarDealer.DTOs.Export;
+
+using System.Xml.Serialization;
+
+[XmlType("customer")]
+public class ExportTotalSalesByCustomer
+{
+
+    [XmlAttribute("full-name")]
+    public string FullName { get; set;}
+
+    [XmlAttribute("bought-cars")]
+    public int BoughtCarsCount { get; set; }
+
+    [XmlIgnore]
+    public bool IsYoungDriver { get; set; }
+
+    [XmlAttribute("spent-money")]
+    public string TotalMoneySpent { get; set; }
+
+}
---
+++ XML_Exercise/19. Export Sales With Applied Discount/CarDealer/DTOs/Import/ImportSalesDto.cs
@@ -0,0 +1,16 @@
+﻿namespace CarDealer.DTOs.Import;
+
+using System.Xml.Serialization;
+
+[XmlType("Sale")]
+public class ImportSalesDto
+{
+    [XmlElement("carId")]
+    public int CarId { get; set; }
+
+    [XmlElement("customerId")]
+    public int CustomerId { get; set; }
+
+    [XmlElement("discount")]
+    public int Discount { get; set; }
+}
