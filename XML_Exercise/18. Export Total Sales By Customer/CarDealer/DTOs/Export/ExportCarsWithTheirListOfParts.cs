7a5c6a7dd76a8c48cada021c9c56c9069cc92c24
ZhechoZhechev <mishomishev024@gmail.com>
2023-03-17 15:44:59 +0200

    XML Exercise

---
+++ XML_Exercise/18. Export Total Sales By Customer/CarDealer/DTOs/Export/ExportCarsWithTheirListOfParts.cs
@@ -0,0 +1,30 @@
+﻿
+using System.Xml.Serialization;
+
+namespace CarDealer.DTOs.Export;
+
+[XmlType("car")]
+public class ExportCarsWithTheirListOfParts
+{
+    [XmlAttribute("make")]
+    public string Make { get; set; }
+
+    [XmlAttribute("model")]
+    public string Model { get; set; }
+
+    [XmlAttribute("traveled-distance")]
+    public long TraveledDistance { get; set; }
+
+    [XmlArray("parts")]
+    public PartExpDto[] ListParts { get; set; }
+}
+
+[XmlType("part")]
+public class PartExpDto 
+{
+    [XmlAttribute("name")]
+    public string Name { get; set; }
+
+    [XmlAttribute("price")]
+    public decimal Price { get; set; }
+}
---
+++ XML_Exercise/19. Export Sales With Applied Discount/CarDealer/DTOs/Export/ExportSalesWithAppliedDiscount.cs
@@ -0,0 +1,46 @@
+﻿namespace CarDealer.DTOs.Export;
+
+using System.Xml.Serialization;
+
+[XmlType("sale")]
+public class ExportSalesWithAppliedDiscount
+{
+    [XmlElement("car")]
+    public SingleCarDto Car { get; set; }
+
+    [XmlElement("discount")]
+    public decimal Discount { get; set; }
+
+    [XmlElement("customer-name")]
+    public string CustomerName { get; set; }
+
+    [XmlElement("price")]
+    public decimal Price { get; set; }
+
+    [XmlElement("price-with-discount")]
+    public double PriceWithDiscount
+    {
+        get
+        {
+            return Math.Round((double)(this.Price * (1-this.Discount / 100)), 4);
+        }
+        set
+        {
+        }
+    }
+
+
+}
+
+[XmlType("car")]
+public class SingleCarDto
+{
+    [XmlAttribute("make")]
+    public string Make { get; set; }
+
+    [XmlAttribute("model")]
+    public string Model { get; set; }
+
+    [XmlAttribute("traveled-distance")]
+    public long TraveledDistance { get; set; }
+}
