7e79091386c6674f1bc222249bd5ea9cea9ec7b8
ZhechoZhechev <mishomishev024@gmail.com>
2023-03-23 11:35:08 +0200

    Exam Preparation

--- Exam Prep/Retake Exam - 15 August 2022/Trucks/DataProcessor/Serializer.cs
+++ Exam Prep/Retake Exam - 15 August 2022/Trucks/DataProcessor/Serializer.cs
@@ -1,21 +1,87 @@
 ﻿namespace Trucks.DataProcessor
 {
     using System;
-
+    using System.IO;
+    using System.Linq;
+    using System.Text;
+    using System.Xml.Serialization;
     using Data;
-
-    using Formatting = Newtonsoft.Json.Formatting;
+    using Newtonsoft.Json;
+    using Trucks.DataProcessor.ExportDto;
 
     public class Serializer
     {
         public static string ExportDespatchersWithTheirTrucks(TrucksContext context)
         {
-            throw new NotImplementedException();
+            var despWithTrucksDtos = context.Despatchers
+                .Where(d => d.Trucks.Any())
+                .ToArray()
+                .Select(d => new ExportDespatchersWithTheirTrucksDto()
+                {
+                    TrucksCount = d.Trucks.Count(),
+                    DespatcherName = d.Name,
+                    Trucks = d.Trucks
+                    .Select(t => new ExportTrucksDto()
+                    {
+                        RegistrationNumber = t.RegistrationNumber,
+                        Make = t.MakeType.ToString()
+                    })
+                    .OrderBy(t => t.RegistrationNumber)
+                    .ToArray()
+                })
+                .OrderByDescending(d => d.TrucksCount)
+                .ThenBy(d => d.DespatcherName)
+                .ToArray();
+
+            var output = Serialize<ExportDespatchersWithTheirTrucksDto[]>(despWithTrucksDtos, "Despatchers");
+            return output;
         }
 
         public static string ExportClientsWithMostTrucks(TrucksContext context, int capacity)
         {
-            throw new NotImplementedException();
+            var clientsWithTrucks = context.Clients
+                .Where(c => c.ClientsTrucks.Any(t => t.Truck.TankCapacity >= capacity))
+                .ToArray()
+                .Select(c => new
+                {
+                    Name = c.Name,
+                    Trucks = c.ClientsTrucks.Where(t => t.Truck.TankCapacity >= capacity)
+                    .Select(t => new
+                    {
+                        TruckRegistrationNumber = t.Truck.RegistrationNumber,
+                        VinNumber = t.Truck.VinNumber,
+                        TankCapacity = t.Truck.TankCapacity,
+                        CargoCapacity = t.Truck.CargoCapacity,
+                        CategoryType = t.Truck.CategoryType.ToString(),
+                        MakeType = t.Truck.MakeType.ToString()
+                    })
+                    .OrderBy(t => t.MakeType)
+                    .ThenByDescending(t => t.CargoCapacity)
+                    .ToArray()
+
+                })
+                .OrderByDescending(c => c.Trucks.Count())
+                .ThenBy(c => c.Name)
+                .Take(10)
+                .ToArray();
+
+            var output = JsonConvert.SerializeObject(clientsWithTrucks, Formatting.Indented);
+            return output;
+        }
+
+        private static string Serialize<T>(T dataTransferObjects, string xmlRootAttributeName)
+        {
+            XmlSerializer serializer = new XmlSerializer(typeof(T), new XmlRootAttribute(xmlRootAttributeName));
+
+            StringBuilder sb = new StringBuilder();
+            using var write = new StringWriter(sb);
+
+            XmlSerializerNamespaces xmlNamespaces = new XmlSerializerNamespaces();
+            xmlNamespaces.Add(string.Empty, string.Empty);
+
+            serializer.Serialize(write, dataTransferObjects, xmlNamespaces);
+
+            return sb.ToString();
         }
     }
 }
---
+++ Exam Prep/Retake Exam - 15 August 2022/Trucks/DataProcessor/exportdto/ExportDespatchersWithTheirTrucksDto.cs
@@ -0,0 +1,27 @@
+﻿namespace Trucks.DataProcessor.ExportDto
+{
+    using System.Xml.Serialization;
+
+    [XmlType("Despatcher")]
+    public class ExportDespatchersWithTheirTrucksDto
+    {
+        [XmlAttribute("TrucksCount")]
+        public int TrucksCount { get; set; }
+
+        [XmlElement("DespatcherName")]
+        public string DespatcherName { get; set; }
+
+        [XmlArray]
+        public ExportTrucksDto[] Trucks { get; set; }
+    }
+
+    [XmlType("Truck")]
+    public class ExportTrucksDto 
+    {
+        [XmlElement("RegistrationNumber")]
+        public string RegistrationNumber { get; set; }
+
+        [XmlElement("Make")]
+        public string Make { get; set;}
+    }
+}
