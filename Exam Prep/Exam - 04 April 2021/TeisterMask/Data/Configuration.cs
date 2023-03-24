678636b28c811dbc6fefe1c84ad26ce1909fdadd
ZhechoZhechev <mishomishev024@gmail.com>
2023-03-24 18:10:19 +0200

    Exam Preparation

---
+++ Exam Prep/Exam - 04 April 2021/TeisterMask/Data/Configuration.cs
@@ -0,0 +1,7 @@
+﻿namespace TeisterMask.Data
+{
+    public static class Configuration
+    {
+        public static string ConnectionString = @"Server=.;Database=TeisterMaskDB;User Id=sa;Password=8201291108;Trusted_Connection=False;MultipleActiveResultSets=true; TrustServerCertificate=true;";
+    }
+}
