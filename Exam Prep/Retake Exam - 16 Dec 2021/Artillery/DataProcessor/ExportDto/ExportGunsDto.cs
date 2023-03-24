eef4f87494ba63c39bd9285a46f6db16a6cf50e3
ZhechoZhechev <mishomishev024@gmail.com>
2023-03-24 14:30:57 +0200

    Exam Preparation

---
+++ Exam Prep/Retake Exam - 16 Dec 2021/Artillery/DataProcessor/ExportDto/ExportGunsDto.cs
@@ -0,0 +1,36 @@
+﻿using System.Xml.Serialization;
+
+namespace Artillery.DataProcessor.ExportDto
+{
+    [XmlType("Gun")]
+    public class ExportGunsDto
+    {
+        [XmlAttribute("Manufacturer")]
+        public string Manufacturer { get; set; }
+
+        [XmlAttribute("GunType")]
+        public string GunType { get; set; }
+
+        [XmlAttribute("GunWeight")]
+        public string GunWeight { get; set; }
+
+        [XmlAttribute("BarrelLength")]
+        public string BarrelLength { get; set; }
+
+        [XmlAttribute("Range")]
+        public string Range { get; set; }
+
+        [XmlArray]
+        public ExportCountryDto[] Countries { get; set; }
+    }
+
+    [XmlType("Country")]
+    public class ExportCountryDto 
+    {
+        [XmlAttribute("Country")]
+        public string Country { get; set; }
+
+        [XmlAttribute("ArmySize")]
+        public string ArmySize { get; set; }
+    }
+}
--- Exam Prep/Retake Exam - 16 Dec 2021/Artillery/DataProcessor/Serializer.cs
+++ Exam Prep/Retake Exam - 16 Dec 2021/Artillery/DataProcessor/Serializer.cs
@@ -2,18 +2,87 @@
 namespace Artillery.DataProcessor
 {
     using Artillery.Data;
+    using Artillery.DataProcessor.ExportDto;
+    using Newtonsoft.Json;
     using System;
+    using System.IO;
+    using System.Linq;
+    using System.Text;
+    using System.Xml.Serialization;
 
     public class Serializer
     {
         public static string ExportShells(ArtilleryContext context, double shellWeight)
         {
-            throw new NotImplementedException();
+            var shellGunsDtos = context.Shells
+                .Where(s => s.ShellWeight > shellWeight)
+                .ToArray()
+                .Select(s => new 
+                {
+                    s.ShellWeight,
+                    s.Caliber,
+                    Guns = s.Guns
+                    .Where(g => g.GunType.ToString() == "AntiAircraftGun")
+                    .Select (g => new
+                    {
+                        GunType = g.GunType.ToString(),
+                        g.GunWeight,
+                        g.BarrelLength,
+                        Range = g.Range > 3000 ? "Long-range" : "Regular range"
+                    })
+                    .OrderByDescending(g => g.GunWeight)
+                    .ToArray()
+                })
+                .OrderBy(s => s.ShellWeight)
+                .ToArray();
+
+            var output = JsonConvert.SerializeObject(shellGunsDtos, Formatting.Indented);
+            return output;
         }
 
         public static string ExportGuns(ArtilleryContext context, string manufacturer)
         {
-            throw new NotImplementedException();
+            var gunCountriesDtos = context.Guns
+                .Where(g => g.Manufacturer.ManufacturerName == manufacturer)
+                .ToArray()
+                .OrderBy(g => g.BarrelLength)
+                .Select(g => new ExportGunsDto()
+                {
+                    Manufacturer = g.Manufacturer.ManufacturerName,
+                    GunType = g.GunType.ToString(),
+                    GunWeight = g.GunWeight.ToString(),
+                    BarrelLength = g.BarrelLength.ToString(),
+                    Range = g.Range.ToString(),
+                    Countries = g.CountriesGuns
+                    .Where(c => c.Country.ArmySize > 4500000)
+                    .Select(c => new ExportCountryDto()
+                    {
+                        Country = c.Country.CountryName,
+                        ArmySize = c.Country.ArmySize.ToString()
+                    })
+                    .OrderBy(c => c.ArmySize)
+                    .ToArray()
+                })
+                .ToArray();
+
+            var output = Serialize<ExportGunsDto[]>(gunCountriesDtos, "Guns");
+            return output;
+                
+        }
+
+        private static string Serialize<T>(T dataTransferObjects, string xmlRootAttributeName)
+        {
+            XmlSerializer serializer = new XmlSerializer(typeof(T), new XmlRootAttribute(xmlRootAttributeName));
+
+            StringBuilder sb = new StringBuilder();
+            using var write = new StringWriter(sb);
+
+            XmlSerializerNamespaces xmlNamespaces = new XmlSerializerNamespaces();
+            xmlNamespaces.Add(string.Empty, string.Empty);
+
+            serializer.Serialize(write, dataTransferObjects, xmlNamespaces);
+
+            return sb.ToString();
         }
     }
 }
