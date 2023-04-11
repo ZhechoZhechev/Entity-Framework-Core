d84249881d6e026136d1858f9ac8b34189359384
ZhechoZhechev <mishomishev024@gmail.com>
2023-04-11 15:40:54 +0300

    Eventmi Workshop

--- EventmiWorkshop/Eventmi/Eventmi/Controllers/EventController.cs
+++ EventmiWorkshop/Eventmi/Eventmi/Controllers/EventController.cs
@@ -35,20 +35,20 @@ public class EventController : Controller
     }
 
     [HttpGet]
-    public IActionResult Add() 
+    public IActionResult Add()
     {
-        var model = new EventDto() 
+        var model = new EventDto()
         {
             Start = DateTime.Today,
             End = DateTime.Today
         };
 
-        return View("Add", model); 
+        return View("Add", model);
     }
 
     [HttpPost]
     [ValidateAntiForgeryToken]
-    public async Task<IActionResult> Add(EventDto model) 
+    public async Task<IActionResult> Add(EventDto model)
     {
         if (!ModelState.IsValid)
         {
@@ -69,15 +69,86 @@ public class EventController : Controller
     }
 
     [HttpGet]
-    public async Task<IActionResult> Details(int id) 
+    public async Task<IActionResult> Details(int id)
     {
-        EventDto eventDto;
+        EventDto model;
 
         try
         {
-            await eventService.GetEventAsync(id);
+            model = await eventService.GetEventAsync(id);
+
+            return View(model);
         }
-        catch(ArgumentException aex)
+        catch (ArgumentException aex)
+        {
+            ViewBag.ErrorMassages = aex.Message;
+        }
+        catch (Exception ex)
+        {
+            logger.LogError("EventController/Details", ex);
+            ViewBag.ErrorMassage = "Възникна непредвидена грешка!";
+        }
+
+        return RedirectToAction(nameof(Index));
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Delete(int id) 
+    {
+
+        try
+        {
+            await eventService.DeleteAsync(id);
+        }
+        catch (Exception ex)
+        {
+            logger.LogError("EventController/Delete", ex);
+            ViewBag.ErrorMassage = "Възникна непредвидена грешка!";
+        }
+
+        return RedirectToAction(nameof(Index));
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> Edit(int id) 
+    {
+        EventDto model;
+
+        try
+        {
+            model = await eventService.GetEventAsync(id);
+
+            return View(model);
+        }
+        catch (ArgumentException aex)
+        {
+            ViewBag.ErrorMassages = aex.Message;
+        }
+        catch (Exception ex)
+        {
+            logger.LogError("EventController/Details", ex);
+            ViewBag.ErrorMassage = "Възникна непредвидена грешка!";
+        }
+
+        return RedirectToAction(nameof(Index));
+    }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Edit(EventDto model) 
+    {
+        if (!ModelState.IsValid) 
+        {
+            return View(model);
+        }
+
+        try
+        {
+            await eventService.UpdateAsync(model);
+
+            return RedirectToAction(nameof(Details), new {id = model.Id });
+        }
+        catch (ArgumentException aex)
         {
             ViewBag.ErrorMassages = aex.Message;
         }
